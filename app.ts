0608a34ecfed87cbc90c9aaf1696c58b1705b3d8
Benson Chen <tumlivein@gmail.com>
2019-10-08 15:49:47 +0800

    6-3 Applying generic constraints: some problems

--- app.ts
+++ app.ts
@@ -1,46 +1,6 @@
-import chalk from 'chalk';
-
-var array: number[] = [1, 2, 3];
-var array2: Array<number> = [1, 2, 3];
-
-class KeyValuePair<TKey, TValue> {
-    constructor (
-        public key: TKey,
-        public value: TValue,
-    ) {}
-
-    public getKey() {
-        console.log(chalk.black.bgBlue(` ${this.key} `));
-    }
-
-    public getValue() {
-        console.log(chalk.black.bgYellow(` ${this.value} `));
-    }
+function totalLength(x: { length: number }, y: { length: number }) {
+    var total: number = x.length + y.length;
+    return total;
 }
 
-let pair1 = new KeyValuePair<number, string>(1, 'First');
-pair1.getKey();
-pair1.getValue();
-
-let pair2 = new KeyValuePair<string, Date>('Second', new Date());
-pair2.getKey();
-pair2.getValue();
-
-let pair3 = new KeyValuePair<number, string>(3, 'Third');
-pair3.getKey();
-pair3.getValue();
-
-
-class KeyValuePairPrinter<T, U> {
-    constructor(private paris: KeyValuePair<T, U>[]) {}
-
-    print() {
-        for(let p of this.paris) {
-            console.log(`${p.key}: ${p.value}`);
-        }
-    }
-}
-
-// var printer = new KeyValuePairPrinter([ pair1, pair2 ]);  // fail -> pair1 & pair2 are not the same type
-var printer = new KeyValuePairPrinter([ pair1, pair3 ]);
-printer.print();
+var length = totalLength('Jess', [1,2,3]);
