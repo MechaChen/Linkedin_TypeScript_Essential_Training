e125cad49bd60e48ff84cbcea737e5c479ee8453
Benson Chen <tumlivein@gmail.com>
2019-10-08 16:15:41 +0800

    7-2 Organizing your code with namespaces: with Namespace

--- model.js
+++ model.js
@@ -1,7 +1,13 @@
-var TodoState;
-(function (TodoState) {
-    TodoState[TodoState["New"] = 1] = "New";
-    TodoState[TodoState["Active"] = 2] = "Active";
-    TodoState[TodoState["Completed"] = 3] = "Completed";
-    TodoState[TodoState["Deleted"] = 4] = "Deleted";
-})(TodoState || (TodoState = {}));
+var TodoApp;
+(function (TodoApp) {
+    var Model;
+    (function (Model) {
+        var TodoState;
+        (function (TodoState) {
+            TodoState[TodoState["New"] = 1] = "New";
+            TodoState[TodoState["Active"] = 2] = "Active";
+            TodoState[TodoState["Completed"] = 3] = "Completed";
+            TodoState[TodoState["Deleted"] = 4] = "Deleted";
+        })(TodoState || (TodoState = {}));
+    })(Model = TodoApp.Model || (TodoApp.Model = {}));
+})(TodoApp || (TodoApp = {}));
--- model.ts
+++ model.ts
@@ -1,19 +1,23 @@
-interface Todo {
-    id: number;
-    name: string;
-    state: TodoState;
+namespace TodoApp.Model {
+    interface Todo {
+        id: number;
+        name: string;
+        state: TodoState;
+    }
+
+    enum TodoState {
+        New = 1,
+        Active,
+        Completed,
+        Deleted,
+    }
+
+    interface ITodoService {
+        add(todo: Todo): Todo;
+        delete(todoId: number): void;
+        getAll(): Todo[];
+        getById(todoId: number): Todo;
+    }
 }
 
-enum TodoState {
-    New = 1,
-    Active,
-    Completed,
-    Deleted,
-}
 
-interface ITodoService {
-    add(todo: Todo): Todo;
-    delete(todoId: number): void;
-    getAll(): Todo[];
-    getById(todoId: number): Todo;
-}
