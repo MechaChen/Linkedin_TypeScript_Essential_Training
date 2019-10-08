2ec04d59c455616f4c6367b437e058609cd00d67
Benson Chen <tumlivein@gmail.com>
2019-10-08 16:57:05 +0800

    7-7 Importing modules using ECMAScript 2015

--- app.ts
+++ app.ts
@@ -1,12 +1,1 @@
-interface IHaveALength {
-    length: number;
-}
-
-function totalLength<T extends IHaveALength>(x: T, y: T) {
-    var total: number = x.length + y.length;
-    return total;
-}
-
-class CustomArray<T> extends Array<T> {}
-
-var length = totalLength([1, 2], new CustomArray());
+import { Todo, TodoState } from './model';
